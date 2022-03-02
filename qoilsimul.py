1cd2de97eaf89c22d84e4685bd07e9fc0070d66b
luyves <knz@tutanota.com>
2022-03-02 11:52:29 +1000

    Detector class (v.1) and circuit drawing methods

--- qoilsimul.py
+++ qoilsimul.py
@@ -15,17 +15,15 @@ def co(*args):
 class Experiment:
     def __init__(self):
         self.sources = []
-        self.objects = []
+        self.elements = {}
         self.detectors = []
-        self.step = 0
-        self.totalSteps = 0
+        self.runs = 1
         print('Experiment initialised.')
 
     def emptyMode(self):
         m = sp.symbols(next(self._ltr),cls=sp.IndexedBase)
         self.pathModes.append(m)
         return m
-
 
     def addModes(self,*args):
         self.modes = [mode for mode in args]
@@ -35,7 +33,10 @@ class Experiment:
         self.generateState()
 
     def addElements(self,*args):
-        self.timeline = {i:arg for i,arg in enumerate(args)}
+        self.elements = {(i+1):arg for i,arg in enumerate(args)}
+
+    def addDetectors(self,detectors):
+        self.detectors = detectors
 
     def generateState(self):
         photons = self.sources
@@ -62,11 +63,74 @@ class Experiment:
                 next_[sp.symbols(m,cls=sp.IndexedBase)] = None
             photonStates.append(state)
             photon.o = modes
-            photon.next = next_
         try:
             self.state = reduce(mul, photonStates)
         except TypeError:
             raise('No photon states!')
+
+    def generateCircuit(self):
+        if len(self.sources) == 0:
+            print('No photon sources in the experiment.')
+        elif len(self.elements) == 0:
+            print('No optical elements in the circuit.')
+        else:
+            self.circuit = ''
+            for m in self.pathModes:
+                step = '...'
+                st = '{} : '.format(m)
+                if m in list(chain.from_iterable([p.o for p in self.sources])):
+                    step = '---'
+                    st += 'p -'
+                else:
+                    st += step
+                for op in self.elements.values():
+                    st += step
+                    if m in op.i:
+                        step = '---'
+                        st+= op.label.replace(' ','-')
+                    else:
+                        st += step
+                st += step
+
+                if m in self.detectors.i:
+                    st += 'D'
+                else:
+                    st += '-'
+                self.circuit += st + '\n'
+
+    def build(self):
+        states = {}
+        # Run experiment
+        self.generateState()
+        states[0] = self.state
+        for i,element in self.elements.items():
+            element.genUnitary(self.state)
+            self.state = self.state.subs(element.u,simultaneous=True)
+            states[i] = self.state
+        self.result = states
+        self.coincidence()
+
+    def simulate(self):
+        print('Simulating experiment...')
+        print('> Total runs: ',self.runs)
+
+        resultsDict = dict()
+        resultsDict['sources'] = {i:photon.state for i,photon in enumerate(self.sources)}
+        resultsDict['elements'] = {i:[element.label,
+                                      element.dof] for i,element in self.elements.items()}
+        resultsDict['data'] = dict()
+        for run in range(self.runs):
+            individualRunDict = dict()
+            self.build()
+            individualRunDict['timeline'] = self.result
+            individualRunDict['startState'] = self.result[0]
+            resultsDict['data'][run] = individualRunDict
+        print('Simulation done.')
+        self.summary = resultsDict
+
+    def coincidence(self):
+        self.detectors.coincidence(self.state)
+        self.successProbability = self.detectors.coincidenceProb
 
 class Photon:
     instances = []
@@ -84,6 +148,28 @@ class Photon:
         else:
             self.mixed = False
         self.state = state
+
+class Detectors:
+    def __init__(self,numberOfDetectors):
+        self.id = id(self)
+        self.i = np.array([None]*numberOfDetectors,dtype=object)
+        self.numberOfDetectors = numberOfDetectors
+
+    def coincidence(self,state):
+        if not any(self.i):
+            print('No detectors connected')
+            return
+        else:
+            prob = 0
+            for arg in sp.preorder_traversal(state.expand()):
+                if arg.is_Mul:
+                    amp,factors = sp.factor_list(arg)
+                    idx = []
+                    for f in factors:
+                        idx += f[0].indices
+                    if set(self.i).issubset(set(idx)):
+                        prob += amp**2
+            self.coincidenceProb = prob
 
 class Element:
     _ref = {'path':None,'pol':[H,V]}
@@ -169,8 +255,9 @@ class BS(Element):
     def __init__(self):
         dof = ['path']
         pathmodes = 2
-        rule = {p1: lambda a,b: co(*a)+co(*b),
-                p2: lambda a,b: co(*a)-co(*b)}
+        s = (1/sp.sqrt(2))
+        rule = {p1: lambda a,b: s*(co(*a)+co(*b)),
+                p2: lambda a,b: s*(co(*a)-co(*b))}
         self.label = 'BS '
         Element.__init__(self,rule,dof,pathmodes)
 
@@ -178,7 +265,6 @@ class HWP(Element):
     def __init__(self,th):
         dof = ['pol']
         pathmodes = 1
-        s = (1/sp.sqrt(2))
         rule = {H: lambda H,V: sp.cos(2*th)*co(*H)+sp.sin(2*th)*co(*V),
                 V: lambda H,V: sp.sin(2*th)*co(*H)-sp.cos(2*th)*co(*V)}
         self.label = 'HWP'
