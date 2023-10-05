38b0f7b566efc4927e4b6a1cb86d81cb3e2135fc
luyves <knz@tutanota.com>
2023-10-05 11:08:16 +1000

    New branch for revisions on element rules

--- qoilsimul.py
+++ qoilsimul.py
@@ -7,10 +7,11 @@ import random
 from itertools import product,chain
 
 H,V = sp.symbols('H V', cls=sp.IndexedBase)
-p1,p2 = sp.symbols('p1 p2', cls=sp.IndexedBase)
+p1, p2 = sp.symbols('p1 p2', cls=sp.IndexedBase)
+# sp.symbols('p1:20',cls_=sp.IndexedBase)
 
 def co(*args):
-    return sp.symbols('x',cls=sp.IndexedBase)[args]
+    return sp.symbols('x', cls=sp.IndexedBase)[args]
 
 class Experiment:
     def __init__(self):
@@ -21,9 +22,12 @@ class Experiment:
         print('Experiment initialised.')
 
     def emptyMode(self):
-        m = sp.symbols(next(self._ltr),cls=sp.IndexedBase)
+        m = sp.symbols(next(self._ltr), cls=sp.IndexedBase)
         self.pathModes.append(m)
         return m
+
+    def nextMode(self, pathmode):
+        pass
 
     def addModes(self,*args):
         self.modes = [mode for mode in args]
@@ -233,6 +237,50 @@ class Detectors:
             self.finalState = projState
 
 class Element:
+    """
+    Base class representing an optical element.
+
+    The Element class serves as the base class for different types of optical elements. It provides basic functionality and methods used by various optical elements.
+
+    Parameters:
+        rule (dict): A dictionary defining the rules for the optical element. The keys represent specific combinations
+                     of input modes, and the values are lambda functions that calculate the output state based on the
+                     given input state(s).
+        dof (dict): A dictionary representing the degrees of freedom associated with the optical element. The 'path' key
+                    is reserved to track the selected path/mode through the element.
+        pathmodes (int): An integer representing the number of paths/modes in the optical element.
+
+    Attributes:
+        id (int): The unique identifier for the optical element.
+        rule (dict): A dictionary defining the rules for the optical element.
+        dof (dict): A dictionary representing the degrees of freedom associated
+                    with the optical element.
+        pathmodes (int): An integer representing the number of paths/modes in
+                    the optical element.
+        o (numpy array): An array representing the output state of the optical
+                    element.
+        _i (numpy array): An internal array representing the input state of the
+                    optical element.
+        u (dict): A dictionary containing the unitary operation associated with
+                    the optical element.
+
+    Properties:
+        i (numpy array): The input state of the optical element.
+
+    Methods:
+        replaceTupleVal(tup, ix, val): Helper method to replace values in a tuple at specific indices.
+        genIdx(modes, indices): Helper method to generate new indices based on selected modes.
+        genUnitary(state): Generates unitary operations based on the input state and the element's rule.
+        updateRules(newKeys=None, verbose=False): Updates the rule of the optical element with new keys if provided.
+        prettify(): Prints a prettified version of the rule for the optical element.
+
+    Example:
+        # Creating an instance of the Element class
+        >>> rule = {('p1',): lambda a: co(*a) * (1 + 2j), ('p2',): lambda b: co(*b) * (3 - 4j)}
+        >>> dof = {'path': None, 'some_other_param': 42}
+        >>> pathmodes = 2
+        >>> element = Element(rule, dof, pathmodes)
+    """
     def __init__(self, rule, dof, pathmodes):
         self.id = id(self)
         self.rule = rule
@@ -241,6 +289,7 @@ class Element:
         self.pathmodes = pathmodes
         self.o = np.array([None]*pathmodes,dtype=object)
         self._i = np.array([None]*pathmodes,dtype=object)
+        self.test = 0
 
     @property
     def i(self):
@@ -332,7 +381,7 @@ class QWP(Element):
 
 class BS(Element):
     def __init__(self,r=1/sp.Rational(2)):
-        dof = {'path':[p1,p2]}
+        dof = {'path':None}
         pathmodes = 2
         rs = sp.sqrt(r)
         ts = sp.sqrt(1-r)
@@ -343,7 +392,7 @@ class BS(Element):
 
 class PBS(Element):
     def __init__(self):
-        dof = {'path':[p1,p2],'pol':[H,V]}
+        dof = {'pol':[H,V]}
         pathmodes = 2
         rule = {(p1,H): lambda aH,aV,bH,bV: co(*aH),
                 (p1,V): lambda aH,aV,bH,bV: sp.I*co(*bV),
@@ -354,21 +403,20 @@ class PBS(Element):
 
 class UBS(Element):
     """ Universal beam splitter
-    
     """
-    def __init__(self, user_modes):
-        dof = {'user':user_modes}
+    def __init__(self, user_modes, label="UBS", dof_label='user'):
+        dof = {dof_label:user_modes}
         pathmodes = 2
         rule = {(p1,user_modes[0]): lambda at1,at2,bt1,bt2: co(*at1),
                 (p1,user_modes[1]): lambda at1,at2,bt1,bt2: sp.I*co(*bt2),
                 (p2,user_modes[0]): lambda at1,at2,bt1,bt2: co(*bt1),
                 (p2,user_modes[1]): lambda at1,at2,bt1,bt2: sp.I*co(*at2)}
-        self.label = 'UBS'
+        self.label = label
         Element.__init__(self, rule, dof, pathmodes)
 
 class PPBSH(Element):
     def __init__(self,r = 1/sp.Rational(2)):
-        dof = {'path':[p1,p2],'pol':[H,V]}
+        dof = {'pol':[H,V]}
         pathmodes = 2
         sr = sp.sqrt(r)
         st = sp.sqrt(1-r)
@@ -381,7 +429,7 @@ class PPBSH(Element):
 
 class PPBSV(Element):
     def __init__(self,r = 1/sp.Rational(2)):
-        dof = {'path':[p1,p2],'pol':[H,V]}
+        dof = {'pol':[H,V]}
         pathmodes = 2
         sr = sp.sqrt(r)
         st = sp.sqrt(1-r)
@@ -394,7 +442,7 @@ class PPBSV(Element):
 
 class PhaseShifter(Element):
     def __init__(self,theta):
-        dof = {'path':p1}
+        dof = {'path':None}
         pathmodes = 1
         rule = {p1: lambda a: sp.exp(sp.I*theta)*co(*a)}
         self.label = 'PSh'
@@ -402,7 +450,7 @@ class PhaseShifter(Element):
 
 class Attenuator(Element):
     def __init__(self,theta):
-        dof = {'path':p1}
+        dof = {'path':None}
         pathmodes = 1
         rule = {p1: lambda a: theta*co(*a)}
         self.label = 'Att'
@@ -414,4 +462,43 @@ class Mirror(Element):
         pathmodes = 1
         rule = {V: lambda V: -co(*V)}
         self.label = 'M'
+        Element.__init__(self,rule,dof,pathmodes)
+
+class MZI(Element):
+    """
+    Mach-Zehnder Interferometer (MZI) class.
+
+    The MZI class represents a Mach-Zehnder Interferometer optical element. It is a subclass of the Element class.
+
+    Parameters:
+        theta (list of reals): A list of two real numbers representing the phase angles of the MZI.
+
+    Attributes:
+        label (str): The label assigned to this MZI element.
+        rule (dict): A dictionary defining the rules for the MZI element.
+                     It contains two lambda functions representing the behavior of the two paths in the MZI.
+        dof (dict): A dictionary representing the degrees of freedom associated with the MZI.
+                    In this case, it contains a single entry 'path' with a value of None.
+        pathmodes (int): An integer representing the number of paths/modes in the MZI.
+
+    Example:
+        # Creating an instance of the MZI class
+        >>> theta = [0.5, 1.2j]
+        >>> mzi_element = MZI(theta)
+
+    In this example, a Mach-Zehnder Interferometer element is created with phase angles theta = [0.5, 1.2].
+    The MZI element can be used to perform interference-based optical operations.
+
+    Note:
+        - 'p1' and 'p2' refer to path_1 and path_2 variables, respectively, used in the lambda functions defined within the 'rule' dictionary. The lambda functions represent the behavior of the two paths in the Mach-Zehnder Interferometer (MZI).
+        - The 'co' function represents the bosonic creation operator function used in the lambda functions within the 'rule' dictionary. The 'co' function is defined elsewhere.
+    """
+    def __init__(self,theta):
+        dof = {'path':None}
+        pathmodes = 2
+        phase_1 = sp.exp(sp.I*theta[0])
+        phase_2 = sp.exp(sp.I*theta[1])
+        rule = {p1: lambda a,b: co(*a)*(phase_1/2)*(phase_2+1)+co(*b)*(phase_1/2)*(phase_2-1),
+                p2: lambda a,b: co(*a)*(1-phase_2)/2+co(*b)*(1+phase_2)/2}
+        self.label = 'MZI'
         Element.__init__(self,rule,dof,pathmodes)
